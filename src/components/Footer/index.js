92409077a92ee3a46544893de3c0e94df6ea3f61
marisanesmith <marisa@thisworldtraveled.com>
2021-05-02 12:12:14 -0400

    created header and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+    <div>
+        <footer>
+            <div className="footer fixed-bottom bg-light text-center p-2"> Copyright © This World Traveled 2020
+            </div>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <div>
+            <body>
+            <nav className="navbar navbar-expand-lg p-3 navbar-light">
+            <a className="navbar-brand name" href="index.html">Marisa NeSmith</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="portfolio.html">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="index.html"> About <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="photography.html"> Photography </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </body>
+</div>
+        
+    )
+}
+
+export default Header
