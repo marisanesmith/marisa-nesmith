de30b023646ac914c4f56e47950565f4a4715bd4
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-05-07 12:33:46 -0400

    image loading

--- src/pages/Photography/index.js
+++ src/pages/Photography/index.js
@@ -8,7 +8,7 @@ const PhotoImg = [
         alt: "Entalula island in the Philippines with turqouise water and a small stretch of white sand beach that is surrounded by palm trees."
     },
     {
-        image: "./public/images/maldives.jpg",
+        image: "./public/images/home-page.png",
         title: "Maldives",
         alt: "Over the water villas on stilts in the Maldives with an orange and pink sky during sunset"
     },
