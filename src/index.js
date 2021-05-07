3f76cc05b8d145026285495e7381e28f1dcbfc5b
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-05-07 13:06:19 -0400

    updated photos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+const express = require('express');
 
+const app = express();
 
+app.use(express.static("client/build"));
 
 ReactDOM.render(
   <React.StrictMode>
